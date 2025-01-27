dc659135fa537df78b0a1aa241daf68216c2a503
Stina Persson <stpe1901@student.miun.se>
2025-01-27 11:11:38 +0100

    tagit bort bootstrap och börjat styla sidan lite

--- obj/Debug/net9.0/moment1.1_dotnet.AssemblyInfo.cs
+++ obj/Debug/net9.0/moment1.1_dotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("moment1.1_dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ff78fb5fdf5077ff1e6ee28eed98c649619bc72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55a86617edeb83fb22233b518401728b970531d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("moment1.1_dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("moment1.1_dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
